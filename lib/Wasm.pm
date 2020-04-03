924dd83962509f524ff85199b777e93a42ec2122
Graham Ollis <plicease@cpan.org>
2020-04-03 13:15:26 -0600

    some documentation for the base main module

--- lib/Wasm.pm
+++ lib/Wasm.pm
@@ -7,6 +7,22 @@ use 5.008001;
 # ABSTRACT: Write Perl extensions using Wasm
 # VERSION
 
+=head1 DESCRIPTION
+
+The C<Wasm> Perl dist provides tools for writing Perl bindings using WebAssembly (Wasm).
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Wasm::Wasmtime>
+
+Low level interface to C<wasmtime>.
+
+=back
+
+=cut
+
 1;
 
 
