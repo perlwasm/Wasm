00bf6c6203aa15a1a6eb1a34946408960b53b7be
Graham Ollis <plicease@cpan.org>
2020-04-08 09:18:05 -0600

    get_export instance method

--- examples/gcd.pl
+++ examples/gcd.pl
@@ -1,10 +1,9 @@
 use strict;
 use warnings;
 use Path::Tiny qw( path );
-use Wasm::Wasmtime::Store;
-use Wasm::Wasmtime::Module;
-use Wasm::Wasmtime::Instance;
+use Wasm::Wasmtime;
 
 my $store = Wasm::Wasmtime::Store->new;
 my $module = Wasm::Wasmtime::Module->new( file => path(__FILE__)->parent->child('gcd.wat') );
 my $instance = Wasm::Wasmtime::Instance->new($module);
+my $gcd = $instance->get_export('gcd');
