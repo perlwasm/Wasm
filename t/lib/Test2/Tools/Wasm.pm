76356048b3ca4ccbf1fbd8ac944e49fac8124cd9
Graham Ollis <plicease@cpan.org>
2020-04-08 16:59:38 -0600

    add a test module for handy testing

---
+++ t/lib/Test2/Tools/Wasm.pm
@@ -0,0 +1,37 @@
+package Test2::Tools::Wasm;
+
+use strict;
+use warnings;
+use Test2::API qw( context );
+use base qw( Exporter );
+
+our @EXPORT = qw( wasm_func_ok );
+
+sub wasm_func_ok ($$)
+{
+  my $f = shift;
+  my $wat = shift;
+  require Wasm::Wasmtime;
+
+  my $ctx = context();
+  my $name = "function $f";
+
+  local $@ = '';
+  my $module = eval { Wasm::Wasmtime::Module->new(wat => $wat) };
+  return $ctx->fail_and_release($name, "error loading module", "$@") if $@;
+
+  my $instance = eval { Wasm::Wasmtime::Instance->new($module) };
+  return $ctx->fail_and_release($name, "error creating instance", "$@") if $@;
+
+  my $extern = $instance->get_export($f);
+  return $ctx->fail_and_release($name, "no export $f") unless $extern;
+
+  my $kind = $extern->type->kind;
+  return $ctx->fail_and_release($name, "$f is a $kind, expected a func") unless $kind eq 'func';
+
+  $ctx->pass_and_release($name);
+
+  return $extern->as_func;
+}
+
+1;
