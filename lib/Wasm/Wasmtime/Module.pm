a7f17e4119acf5a2dc4d89d71ebd14394b5bc74c
Graham Ollis <plicease@cpan.org>
2020-04-08 04:47:12 -0600

    add basic module class

---
+++ lib/Wasm/Wasmtime/Module.pm
@@ -0,0 +1,65 @@
+package Wasm::Wasmtime::Module;
+
+use strict;
+use warnings;
+use Wasm::Wasmtime::FFI;
+use Wasm::Wasmtime::Store;
+
+# ABSTRACT: Wasmtime module class
+# VERSION
+
+$ffi->mangler(sub { "wasm_module_$_[0]" });
+$ffi->type('opaque' => 'wasm_module_t');
+
+$ffi->attach( new => ['wasm_store_t','wasm_byte_vec_t*'] => 'wasm_module_t' => sub {
+  my $xsub = shift;
+  my $class = shift;
+  my $store = defined $_[0] && ref($_[0]) eq 'Wasm::Wasmtime::Store' ? shift : Wasm::Wasmtime::Store->new;
+  my $wasm;
+  my $data;
+  if(@_ == 1)
+  {
+    $wasm = Wasm::Wasmtime::ByteVec->new(shift);
+  }
+  else
+  {
+    my $key = shift;
+    if($key eq 'wat')
+    {
+      require Wasm::Wasmtime::Wat2Wasm;
+      $data = Wasm::Wasmtime::Wat2Wasm::wat2wasm(shift);
+      $wasm = Wasm::Wasmtime::ByteVec->new($data);
+    }
+    elsif($key eq 'wasm')
+    {
+      $wasm = Wasm::Wasmtime::ByteVec->new(shift);
+    }
+    elsif($key eq 'file')
+    {
+      require Wasm::Wasmtime::Wat2Wasm;
+      require Path::Tiny;
+      $data = Wasm::Wasmtime::Wat2Wasm::wat2wasm(Path::Tiny->new(shift)->slurp_utf8);
+      $wasm = Wasm::Wasmtime::ByteVec->new($data);
+    }
+  }
+  bless {
+    ptr   => $xsub->($store->{ptr}, $wasm),
+    store => $store,
+  }, $class;
+});
+
+$ffi->attach( validate => ['wasm_store_t','wasm_byte_vec_t*'] => 'bool' => sub {
+  # FIXME: dumping core?
+  my $xsub = shift;
+  my $class = shift;
+  my $store = defined $_[0] && ref($_[0]) eq 'Wasm::Wasmtime::Store' ? shift : Wasm::Wasmtime::Store->new;
+  $DB::single = 1;
+  my $wasm = Wasm::Wasmtime::ByteVec->new($_[0]);
+  $xsub->($store, $wasm);
+});
+
+$ffi->attach( [ 'delete' => 'DESTROY' ] => ['wasm_module_t'] => sub {
+  my($xsub, $self) = @_;
+  $xsub->($self->{ptr}) if $self->{ptr};
+});
+
