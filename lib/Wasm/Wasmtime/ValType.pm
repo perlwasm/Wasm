0090712ac6733d9a8c4316c05eeabf99d2420a8b
Graham Ollis <plicease@cpan.org>
2020-04-09 16:18:55 -0600

    standalone valtype

--- lib/Wasm/Wasmtime/ValType.pm
+++ lib/Wasm/Wasmtime/ValType.pm
@@ -10,21 +10,6 @@ use Wasm::Wasmtime::FFI;
 $ffi_prefix = 'wasm_valtype_';
 $ffi->type('opaque' => 'wasm_valtype_t');
 
-=head1 CONSTRUCTORS
-
-=head2 new
-
-=cut
-
-sub new
-{
-  my($class, $ptr, $owner) = @_;
-  bless {
-    ptr   => $ptr,
-    owner => $owner,
-  }, $class;
-}
-
 my %kind = (
   0   => 'i32',
   1   => 'i64',
@@ -33,6 +18,34 @@ my %kind = (
   128 => 'anyref',
   129 => 'funcref',
 );
+
+my %rkind;
+foreach my $key (keys %kind)
+{
+  my $value = $kind{$key};
+  $rkind{$value} = $key;
+}
+
+$ffi->attach( new => ['uint8'] => 'wasm_valtype_t' => sub {
+  my $xsub = shift;
+  my $class = shift;
+  my($ptr, $owner);
+  if($_[0] =~ /^[0-9]+$/)
+  {
+    ($ptr, $owner) = @_;
+  }
+  else
+  {
+    my($kind) = @_;
+    my $kind_num = $rkind{$kind};
+    Carp::croak("no such value type: $kind") unless defined $kind_num;
+    $ptr = $xsub->($kind_num);
+  }
+  bless {
+    ptr   => $ptr,
+    owner => $owner,
+  }, $class;
+});
 
 $ffi->attach( kind => ['wasm_valtype_t'] => 'uint8' => sub {
   my($xsub, $self) = @_;
