0df1a0919f9bae94f78062a5a41b9ed0a63f17e4
Graham Ollis <plicease@cpan.org>
2020-04-09 05:09:42 -0600

    add support for wasmtime_erorr_t class

--- lib/Wasm/Wasmtime/Wat2Wasm.pm
+++ lib/Wasm/Wasmtime/Wat2Wasm.pm
@@ -3,6 +3,7 @@ package Wasm::Wasmtime::Wat2Wasm;
 use strict;
 use warnings;
 use Wasm::Wasmtime::FFI;
+use Wasm::Wasmtime::Error;
 use base qw( Exporter );
 
 # ABSTRACT: Convert WebAssembly Text to Wasm
@@ -12,23 +13,46 @@ our @EXPORT = qw( wat2wasm );
 
 $ffi_prefix = 'wasmtime_';
 
-$ffi->attach( wat2wasm => ['wasm_byte_vec_t*','wasm_byte_vec_t*','wasm_byte_vec_t*'] => 'bool' => sub {
-  my $xsub = shift;
-  my $wat = Wasm::Wasmtime::ByteVec->new($_[0]);
-  my $ret = Wasm::Wasmtime::ByteVec->new;
-  my $error_message = Wasm::Wasmtime::ByteVec->new;
-  if($xsub->($wat, $ret, $error_message))
-  {
-    my $wasm = $ret->get;
-    $ret->delete;
-    return $wasm;
-  }
-  else
-  {
-    my $diag = $error_message->get;
-    $error_message->delete;
-    Carp::croak($diag . "\nwat2wasm error");
-  }
-});
+if($ffi->find_symbol('wasmtime_error_message'))
+{
+  $ffi->attach( wat2wasm => ['wasm_byte_vec_t*','wasm_byte_vec_t*'] => 'wasmtime_error_t' => sub {
+    my $xsub = shift;
+    my $wat = Wasm::Wasmtime::ByteVec->new($_[0]);
+    my $ret = Wasm::Wasmtime::ByteVec->new;
+    my $error = $xsub->($wat, $ret);
+    if($error)
+    {
+      $error = Wasm::Wasmtime::Error->new($error)->message;
+      Carp::croak("$error\nwat2wasm error");
+    }
+    else
+    {
+      my $wasm = $ret->get;
+      $ret->delete;
+      return $wasm;
+    }
+  });
+}
+else
+{
+  $ffi->attach( wat2wasm => ['wasm_byte_vec_t*','wasm_byte_vec_t*','wasm_byte_vec_t*'] => 'bool' => sub {
+    my $xsub = shift;
+    my $wat = Wasm::Wasmtime::ByteVec->new($_[0]);
+    my $ret = Wasm::Wasmtime::ByteVec->new;
+    my $error_message = Wasm::Wasmtime::ByteVec->new;
+    if($xsub->($wat, $ret, $error_message))
+    {
+      my $wasm = $ret->get;
+      $ret->delete;
+      return $wasm;
+    }
+    else
+    {
+      my $diag = $error_message->get;
+      $error_message->delete;
+      Carp::croak($diag . "\nwat2wasm error");
+    }
+  });
+}
 
 1;
