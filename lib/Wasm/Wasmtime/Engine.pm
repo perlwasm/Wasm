1349721f772787b265425524b8e779b043f929a9
Graham Ollis <plicease@cpan.org>
2020-04-10 14:26:03 -0600

    documentation (7)

--- lib/Wasm/Wasmtime/Engine.pm
+++ lib/Wasm/Wasmtime/Engine.pm
@@ -8,8 +8,32 @@ use Wasm::Wasmtime::Config;
 # ABSTRACT: Wasmtime engine class
 # VERSION
 
+=head1 SYNOPSIS
+
+# EXAMPLE: examples/synopsis/engine.pl
+
+=head1 DESCRIPTION
+
+This class represents the main WebAssembly engine.  It can optionally
+be configured with a L<Wasm::Wasmtime::Config> object.
+
+=cut
+
 $ffi_prefix = 'wasm_engine_';
 $ffi->type('opaque' => 'wasm_engine_t');
+
+=head1 CONSTRUCTOR
+
+=head2 new
+
+ my $engine = Wasm::Wasmtime::Engine->new;
+ my $engine = Wasm::Wasmtime::Engine->new(
+   $config, # Wasm::Wasmtime::Config
+ );
+
+Creates a new instance of the engine class.
+
+=cut
 
 $ffi->attach( [ 'new_with_config' => 'new' ] => ['wasm_config_t'] => 'wasm_engine_t' => sub {
   my($xsub, $class, $config) = @_;
