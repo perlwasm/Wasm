e4eaabbc063559d4d27eb54043f92431cbdbada6
Graham Ollis <plicease@cpan.org>
2020-04-10 14:58:42 -0600

    documentation (10)

--- lib/Wasm/Wasmtime/MemoryType.pm
+++ lib/Wasm/Wasmtime/MemoryType.pm
@@ -7,8 +7,32 @@ use Wasm::Wasmtime::FFI;
 # ABSTRACT: Wasmtime memory type class
 # VERSION
 
+=head1 SYNOPSIS
+
+# EXAMPLE: examples/synopsis/memorytype.pl
+
+=head1 DESCRIPTION
+
+This class represents a module memory type.  It models the minimum and
+maximum number of pages.
+
+=cut
+
 $ffi_prefix = 'wasm_memorytype_';
 $ffi->type('opaque' => 'wasm_memorytype_t');
+
+=head1 CONSTRUCTOR
+
+=head2 new
+
+ my $memorytype = Wasm::Wasmtime::MemoryType->new([
+   $min,  # minumum number of pages
+   $max   # maximum number of pages
+ ]);
+
+Creates a new memory type object.  
+
+=cut
 
 $ffi->attach( new => ['uint32[2]'] => 'wasm_memorytype_t' => sub {
   my $xsub = shift;
@@ -28,6 +52,14 @@ $ffi->attach( new => ['uint32[2]'] => 'wasm_memorytype_t' => sub {
     owner => $owner,
   }, $class;
 });
+
+=head2 limits
+
+ my $limits = $memorytype->limits;
+
+Returns the minimum and maximum number of pages as an array reference.
+
+=cut
 
 $ffi->attach( limits => ['wasm_memorytype_t'] => 'uint32[2]' => sub {
   my($xsub, $self) = @_;
