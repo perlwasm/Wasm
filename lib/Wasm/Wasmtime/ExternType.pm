7fc72fd386102ef155e0405d81f5c1eb8967fd68
Graham Ollis <plicease@cpan.org>
2020-04-11 08:16:45 -0600

    documentation and tests for extern type

--- lib/Wasm/Wasmtime/ExternType.pm
+++ lib/Wasm/Wasmtime/ExternType.pm
@@ -9,14 +9,19 @@ use Wasm::Wasmtime::MemoryType;
 # ABSTRACT: Wasmtime extern type class
 # VERSION
 
-$ffi_prefix = 'wasm_externtype_';
-$ffi->type('opaque' => 'wasm_externtype_t');
+=head1 SYNOPSIS
 
-=head1 CONSTRUCTORS
+# EXAMPLE: examples/synopsis/externtype.pl
 
-=head2 new
+=head1 DESCRIPTION
+
+This class represents an extern type.  This class cannot be created independently, but can be
+retrieved from the L<Wasm::Wasmtime::Module> class.
 
 =cut
+
+$ffi_prefix = 'wasm_externtype_';
+$ffi->type('opaque' => 'wasm_externtype_t');
 
 sub new
 {
@@ -34,16 +39,69 @@ my %kind = (
   3 => 'memory',
 );
 
+=head1 METHODS
+
+=head2 kind
+
+ my $kind = $externtype->kind;
+
+Returns the kind of extern type.  Should be one of:
+
+=over 4
+
+=item C<func>
+
+=item C<global>
+
+=item C<table>
+
+=item C<memory>
+
+=back
+
+=cut
+
 $ffi->attach( kind => ['wasm_externtype_t'] => 'uint8' => sub {
   my($xsub, $self) = @_;
   $kind{$xsub->($self->{ptr})};
 });
+
+=head2 kind_num
+
+ my $kind = $externtype->kind_num;
+
+Returns the kind of extern type as the internal integer code.
+
+=cut
+
+$ffi->attach( [ kind => 'kind_num' ] => ['wasm_externtype_t'] => 'uint8' => sub {
+  my($xsub, $self) = @_;
+  $xsub->($self->{ptr});
+});
+
+=head2 as_functype
+
+ my $functype = $externtype->as_functype;
+
+If the extern type is a function, returns the L<Was::Wasmtime::FuncType> for it.
+Otherwise returns C<undef>.
+
+=cut
 
 $ffi->attach( as_functype => ['wasm_externtype_t'] => 'wasm_functype_t' => sub {
   my($xsub, $self) = @_;
   my $ptr = $xsub->($self->{ptr});
   $ptr ? Wasm::Wasmtime::FuncType->new($ptr, $self->{owner} || $self) : undef;
 });
+
+=head2 as_memorytype
+
+ my $memorytype = $externtype->as_memorytype;
+
+If the extern type is a memory object, returns the L<Was::Wasmtime::MemoryType> for it.
+Otherwise returns C<undef>.
+
+=cut
 
 $ffi->attach( as_memorytype => ['wasm_externtype_t'] => 'wasm_memorytype_t' => sub {
   my($xsub, $self) = @_;
