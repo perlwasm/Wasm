63f53704723bbbb45532f57aaf38ca6b81bb7506
Graham Ollis <plicease@cpan.org>
2020-04-12 07:26:01 -0600

    allow_shadowing and define methods

--- lib/Wasm/Wasmtime/Linker.pm
+++ lib/Wasm/Wasmtime/Linker.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use Wasm::Wasmtime::FFI;
 use Wasm::Wasmtime::Store;
+use Wasm::Wasmtime::Extern;
+use Ref::Util qw( is_blessed_ref );
 
 # ABSTRACT: Wasmtime linker class
 # VERSION
@@ -40,6 +42,59 @@ $ffi->attach( new => ['wasm_store_t'] => 'wasmtime_linker_t' => sub {
   my($xsub, $class, $store) = @_;
   my $ptr = $xsub->($store->{ptr});
   bless { ptr => $ptr, store => $store }, $class;
+});
+
+=head1 METHODS
+
+=head2 allow_shadowing
+
+ $linker->allow_shadowing($bool);
+
+Sets the allow shadowing property.
+
+=cut
+
+$ffi->attach( allow_shadowing => [ 'wasmtime_linker_t', 'bool' ] => sub {
+  my($xsub, $self, $value) = @_;
+  $xsub->($self->{ptr}, $value);
+  $self;
+});
+
+=head2 define
+
+ $linker->define(
+   $module,
+   $name,
+   $extern,    # Wasm::Wasmtime::Extern
+ );
+
+Define the given extern.  You can use a func, global, table ot memory object in its place
+and this method will get the extern for you.
+
+=cut
+
+$ffi->attach( define => ['wasmtime_linker_t', 'wasm_byte_vec_t*', 'wasm_byte_vec_t*', 'wasm_extern_t'] => 'wasmtime_error_t' => sub {
+  my $xsub   = shift;
+  my $self   = shift;
+  my $module = Wasm::Wasmtime::ByteVec->new(shift);
+  my $name   = Wasm::Wasmtime::ByteVec->new(shift);
+  my $extern = shift;
+
+  if(ref($extern) eq 'Wasm::Wasmtime::Extern')
+  {
+    # nothing, okay.
+  }
+  elsif(is_blessed_ref($extern) && $extern->can('as_extern'))
+  {
+    $extern = $extern->as_extern;
+  }
+  else
+  {
+    Carp::croak("not an extern: $extern");
+  }
+
+  $xsub->($self->{ptr}, $module, $name, $extern->{ptr});
+  $self;
 });
 
 $ffi->attach( [ 'delete' => 'DESTROY' ] => ['wasmtime_linker_t'] => sub {
--- t/wasm_wasmtime_linker.t
+++ t/wasm_wasmtime_linker.t
@@ -1,14 +1,36 @@
 use Test2::V0 -no_srand => 1;
+use lib 't/lib';
+use Test2::Tools::Wasm;
 use Wasm::Wasmtime::Linker;
+
+my $instance = wasm_instance_ok( [], q{
+  (module
+    (func (export "add") (param i32 i32) (result i32)
+      local.get 0
+      local.get 1
+      i32.add)
+    (func (export "sub") (param i64 i64) (result i64)
+      local.get 0
+      local.get 1
+      i64.sub)
+    (memory (export "frooble") 2 3)
+  )
+});
+
+my $module = $instance->module;
+my $store  = $module->store;
 
 is(
   Wasm::Wasmtime::Linker->new(
-    Wasm::Wasmtime::Store->new,
+    $store,,
   ),
   object {
     call [ isa => 'Wasm::Wasmtime::Linker' ] => T();
+    call [ allow_shadowing => 1 ] => D();
+    call [ allow_shadowing => 0 ] => D();
+    call [ define => 'xx', 'add', $instance->get_export('add') ] => D();
   },
-  'basic object create'
+  'basics'
 );
 
 done_testing;
