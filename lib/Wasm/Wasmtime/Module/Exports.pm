330682243454a3b520937a994a2c61b40a5f511c
Graham Ollis <plicease@cpan.org>
2020-05-07 09:08:01 -0600

    starter exports class for modules

---
+++ lib/Wasm/Wasmtime/Module/Exports.pm
@@ -0,0 +1,32 @@
+package Wasm::Wasmtime::Module::Exports;
+
+use strict;
+use warnings;
+
+# ABSTRACT: Wasmtime module exports class
+# VERSION
+
+=head1 SYNOPSIS
+
+# EXAMPLE: examples/synopsis/module_exports.pl
+
+=head1 DESCRIPTION
+
+B<WARNING>: WebAssembly and Wasmtime are a moving target and the interface for these modules
+is under active development.  Use with caution.
+
+TODO
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Wasm>
+
+=item L<Wasm::Wasmtime>
+
+=back
+
+=cut
+
+1;
