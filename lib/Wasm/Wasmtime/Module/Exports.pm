3af8243b3de69fddd99ee21a17c7c3b0e317014d
Graham Ollis <plicease@cpan.org>
2020-05-07 09:47:07 -0600

    autoload methods for module exports

--- lib/Wasm/Wasmtime/Module/Exports.pm
+++ lib/Wasm/Wasmtime/Module/Exports.pm
@@ -2,6 +2,7 @@ package Wasm::Wasmtime::Module::Exports;
 
 use strict;
 use warnings;
+use Carp ();
 
 # ABSTRACT: Wasmtime module exports class
 # VERSION
@@ -16,6 +17,54 @@ B<WARNING>: WebAssembly and Wasmtime are a moving target and the interface for t
 is under active development.  Use with caution.
 
 TODO
+
+=cut
+
+sub new
+{
+  my($class, $module) = @_;
+
+  $module->{exports} ||= do {
+    my @exports = $module->exports;
+    # TODO: lock with Hash::Util
+    my %exports;
+    foreach my $export (@exports)
+    {
+      $exports{$export->name} = $export->type;
+    }
+    \%exports;
+  };
+
+  bless \$module, $class;
+}
+
+sub can
+{
+  my($self, $name) = @_;
+  my $module = $$self;
+  $module->{exports}->{$name}
+    ? sub { $self->$name }
+    : $self->SUPER::can($name);
+}
+
+sub AUTOLOAD
+{
+  our $AUTOLOAD;
+  my $self = shift;
+
+  my $name = $AUTOLOAD;
+  $name=~ s/^.*:://;
+
+  my $module = $$self;
+  my $export = $module->{exports}->{$name};
+  Carp::croak("no export $name") unless $export;
+  $export;
+}
+
+sub DESTROY
+{
+  # needed because of AUTOLOAD
+}
 
 =head1 SEE ALSO
 
--- t/wasm_wasmtime_module_exports.t
+++ t/wasm_wasmtime_module_exports.t
@@ -1,6 +1,29 @@
 use Test2::V0 -no_srand => 1;
+use Wasm::Wasmtime::Module;
 use Wasm::Wasmtime::Module::Exports;
+use YAML qw( Dump );
 
-ok 1;
+{
+  my $module = Wasm::Wasmtime::Module->new(wat => q{
+    (module
+      (func (export "add") (param i32 i32) (result i32)
+        local.get 0
+        local.get 1
+        i32.add)
+    )
+  });
+  my $exports = Wasm::Wasmtime::Module::Exports->new($module);
+  is(
+    $exports,
+    object {
+      call [ isa => 'Wasm::Wasmtime::Module::Exports' ] => T();
+      call add => object {
+        call [ isa => 'Wasm::Wasmtime::FuncType' ] => T();
+      };
+    },
+    'exports object looks good'
+  );
+  note Dump($exports);
+}
 
 done_testing;
