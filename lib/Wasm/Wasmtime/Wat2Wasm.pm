f3655724efeba44eafbc8fc69acb3cbe8284048a
Graham Ollis <plicease@cpan.org>
2020-05-15 11:00:34 -0600

    bump required version of wasmtime

--- lib/Wasm/Wasmtime/Wat2Wasm.pm
+++ lib/Wasm/Wasmtime/Wat2Wasm.pm
@@ -36,46 +36,22 @@ Takes WebAssembly Text C<$wat> and converts it into the WebAssembly binary C<$wa
 
 =cut
 
-if(Wasm::Wasmtime::Error->can('new'))
-{
-  $ffi->attach( wat2wasm => ['wasm_byte_vec_t*','wasm_byte_vec_t*'] => 'wasmtime_error_t' => sub {
-    my $xsub = shift;
-    my $wat = Wasm::Wasmtime::ByteVec->new($_[0]);
-    my $ret = Wasm::Wasmtime::ByteVec->new;
-    my $error = $xsub->($wat, $ret);
-    if($error)
-    {
-      Carp::croak($error->message . "\nwat2wasm error");
-    }
-    else
-    {
-      my $wasm = $ret->get;
-      $ret->delete;
-      return $wasm;
-    }
-  });
-}
-else
-{
-  $ffi->attach( wat2wasm => ['wasm_byte_vec_t*','wasm_byte_vec_t*','wasm_byte_vec_t*'] => 'bool' => sub {
-    my $xsub = shift;
-    my $wat = Wasm::Wasmtime::ByteVec->new($_[0]);
-    my $ret = Wasm::Wasmtime::ByteVec->new;
-    my $error_message = Wasm::Wasmtime::ByteVec->new;
-    if($xsub->($wat, $ret, $error_message))
-    {
-      my $wasm = $ret->get;
-      $ret->delete;
-      return $wasm;
-    }
-    else
-    {
-      my $diag = $error_message->get;
-      $error_message->delete;
-      Carp::croak($diag . "\nwat2wasm error");
-    }
-  });
-}
+$ffi->attach( wat2wasm => ['wasm_byte_vec_t*','wasm_byte_vec_t*'] => 'wasmtime_error_t' => sub {
+  my $xsub = shift;
+  my $wat = Wasm::Wasmtime::ByteVec->new($_[0]);
+  my $ret = Wasm::Wasmtime::ByteVec->new;
+  my $error = $xsub->($wat, $ret);
+  if($error)
+  {
+    Carp::croak($error->message . "\nwat2wasm error");
+  }
+  else
+  {
+    my $wasm = $ret->get;
+    $ret->delete;
+    return $wasm;
+  }
+});
 
 1;
 
