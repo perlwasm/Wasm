aa02414f3f733d776ff4f167f42df58137bc0156
Graham Ollis <plicease@cpan.org>
2020-06-20 07:08:12 -0600

    convert greet example to C

--- examples/wasm/lib/Greet.pm
+++ examples/wasm/lib/Greet.pm
@@ -16,16 +16,14 @@ sub greet
 {
   my($subject) = @_;
 
-  my $input_size   = do { use bytes; length($subject)+1 };
-  my $input_offset = _allocate($input_size);
+  my $input_offset = _allocate(length($subject) + 1);
   strcpy( $memory->address + $input_offset, $subject );
 
   my $output_offset = _greet($input_offset);
   my $greeting      = FFI::Platypus->new->cast('opaque', 'string', $memory->address + $output_offset);
-  my $output_size   = do { use bytes; length($greeting)+1 };
 
-  _deallocate($input_offset,  $input_size);
-  _deallocate($output_offset, $output_size);
+  _deallocate($input_offset);
+  _deallocate($output_offset);
 
   return $greeting;
 }
